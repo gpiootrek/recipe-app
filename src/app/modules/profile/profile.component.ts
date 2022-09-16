80d0ae480ef49cda12dffb4e1e025a647a96dd89
gpiootrek <g.piotreg@gmail.com>
2022-09-16 14:54:43 +0200

    feat: added subscribing to newsletter

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -1,3 +1,6 @@
+import { Observable } from 'rxjs';
+import { map, find, tap, filter } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { User } from './../../core/models/user';
 import {
@@ -7,21 +10,41 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { devOnlyGuardedExpression } from '@angular/compiler';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
+  imports: [CommonModule],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
   @Input() user!: User;
+  isSubscribing$: Observable<boolean>;
 
   constructor(
     public authService: AuthService,
-    private afAuth: AngularFireAuth
-  ) {}
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore
+  ) {
+    this.isSubscribing$ = afs
+      .collection('subscribers')
+      .valueChanges()
+      .pipe(
+        map((val) => {
+          return val.map((doc: any) => doc.email);
+        }),
+        find((emails: any) => {
+          return (
+            emails.findIndex((email: string) => email === this.user.email) !==
+            -1
+          );
+        })
+      );
+  }
 
   ngOnInit(): void {
     this.user = this.authService.GetUserData();
@@ -37,5 +60,17 @@ export class ProfileComponent implements OnInit {
       if (email !== user?.email) user?.updateEmail(email);
       if (password.length > 6) user?.updatePassword(password);
     });
+  }
+
+  onSubscribe() {
+    this.afs.collection('subscribers').add({ email: this.user.email });
+  }
+
+  onUnsubscribe() {
+    // this.afs
+    //   .collection('subscribers')
+    //   .valueChanges()
+    //   .pipe(filter((doc: any) => doc.email !== this.user.email))
+    //   .subscribe();
   }
 }
